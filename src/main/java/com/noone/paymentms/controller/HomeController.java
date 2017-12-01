f7479aec8e0436f2e51b25f8d5097d0095d1405c
ldh <superman_nemo@163.com>
2017-12-01 23:42:14 +0800

    Return JSON success.

--- src/main/java/com/noone/paymentms/controller/HomeController.java
+++ src/main/java/com/noone/paymentms/controller/HomeController.java
@@ -3,13 +3,28 @@ package com.noone.paymentms.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.noone.paymentms.domain.Account;
+
 @RestController
 public class HomeController extends BaseController {
 
+//	@RequestMapping("/hello")
+//	public Account hello() {
+//		System.out.println("hello");
+//		Account account = new Account();
+//		account.setAccountName("account name xxx");
+//		account.setAccountStatus("status");
+//		return account;
+//	}
+	
+	
 	@RequestMapping("/hello")
 	public String hello() {
 		System.out.println("hello");
-		return "hello world!";
+		Account account = new Account();
+		account.setAccountName("account name xxx");
+		account.setAccountStatus("status");
+		return "hello world !";
 	}
 	
 	
