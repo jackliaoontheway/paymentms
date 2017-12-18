df0a65a6210f31cade5e5768d8fc6e3ba1726750
ldh <superman_nemo@163.com>
2017-12-18 21:35:55 +0800

    Added production payment parameters.

--- src/main/java/com/noone/paymentms/biz/pay/util/SwiftpassConfig.java
+++ src/main/java/com/noone/paymentms/biz/pay/util/SwiftpassConfig.java
@@ -6,13 +6,13 @@ public class SwiftpassConfig {
      * 威富通交易密钥
      */
     public static String key = "9d101c97133837e13dde2d32a5054abb"; // test
-//    public static String key = "9c670481313a5c51704b3094c82182da";
+//    public static String key = "40c5ac707c48a83f8f5355afe47571";
     
     /**
      * 威富通商户号
      */
     public static String mch_id = "7551000001"; // test
-//    public static String mch_id = "105580049355";
+//    public static String mch_id = "105540049206";
     
     /**
      * 威富通请求url
